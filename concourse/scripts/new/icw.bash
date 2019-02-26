3d7ad7d9e1c034a41840d2fa08ad35c49e09fb06
Adam Berlin <aberlin@pivotal.io>
2019-02-26 22:09:53 +0000

    Source environment again. oops.

--- concourse/scripts/new/icw.bash
+++ concourse/scripts/new/icw.bash
@@ -18,12 +18,24 @@ export CCACHE_DIR="$PWD/.ccache"
 	    --disable-gpfdist \
 	    ${CONFIGURE_FLAGS};
 
+echo "Make"
 make -j4 -s
+
+echo "Make install"
 make -s install
 
+echo "Source greenplum path"
+source ./greenplum-db-devel/greenplum_path.sh
+
+echo "Start ssh daemon"
 /start-sshd.bash
 
+echo "Create the demo cluster"
 make create-demo-cluster;
+
+echo "Source the demo env"
 source ./gpAux/gpdemo/gpdemo-env.sh
+
+echo "Run installcheck-world"
 make installcheck-world;
 
