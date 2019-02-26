e4572ff52f228f6bd87f16ad509b758190bfd726
Adam Berlin <aberlin@pivotal.io>
2019-02-26 22:30:34 +0000

    Introduce downloading of orca.

--- concourse/scripts/new/icw.bash
+++ concourse/scripts/new/icw.bash
@@ -7,6 +7,30 @@ sudo chown -R $(whoami) $(pwd)
 export INSTALL_DIR="$PWD/greenplum-db-devel/"
 export CCACHE_DIR="$PWD/.ccache"
 
+fetch_and_build_xerces_c() {
+    git clone https://github.com/greenplum-db/gp-xerces /tmp/xerces
+    (
+	    set -e
+	    cd /tmp/xerces
+	    ./configure CC='ccache gcc' CXX='ccache g++' --disable-network
+    )
+    make_install -C /tmp/xerces
+}
+
+fetch_and_build_orca() {
+    fetch_and_build_xerces_c
+    
+    local orca_code_url
+    orca_code_url=$(
+	sed -E -n -e '/gporca/s,.*https://github.com/greenplum-db/gporca/releases/download/v(([[:digit:]]|\.)+)/bin_orca_centos5_release.tar.gz.*,https://github.com/greenplum-db/gporca/archive/v\1.tar.gz,p' <gpAux/releng/releng.mk
+	)
+    mkdir /tmp/orca
+    wget -O - "${orca_code_url}" | tar zx --strip-components=1 -C /tmp/orca
+
+    cmake -GNinja -H/tmp/orca -B/tmp/orca/build
+    ninja install -C /tmp/orca/build
+}
+
 ./configure CC='ccache gcc' \
 	    CXX='ccache g++' \
 	    --prefix=${INSTALL_DIR} \
