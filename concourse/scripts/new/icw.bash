6b6937b564cfad78a26d1bd19c38f1a4880af3d0
Adam Berlin <aberlin@pivotal.io>
2019-02-26 22:02:55 +0000

    Start the ssh daemon before trying to build the demo cluster.

--- concourse/scripts/new/icw.bash
+++ concourse/scripts/new/icw.bash
@@ -20,13 +20,8 @@ export CCACHE_DIR="$PWD/.ccache"
 
 make -j4 -s
 make -s install
-source ./greenplum-db-devel/greenplum_path.sh
 
-{
-  ssh-keyscan localhost
-  ssh-keyscan 0.0.0.0
-  ssh-keyscan `hostname`
-} >> "$HOME/.ssh/known_hosts"
+/start-sshd.bash
 
 make create-demo-cluster;
 source ./gpAux/gpdemo/gpdemo-env.sh
