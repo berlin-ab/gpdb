fd5b226671b40fcd6a0233416457d35d8f8bc464
Adam Berlin <aberlin@pivotal.io>
2019-02-28 19:10:04 +0000

    Refactor runner script.

--- concourse/pr_pipeline/runner.bash
+++ concourse/pr_pipeline/runner.bash
@@ -3,15 +3,6 @@
 set -o errexit
 
 
-export GPDB_INSTALL_DIR="$PWD/greenplum-db-devel/"
-export CCACHE_DIR="$PWD/.ccache"
-
-#
-# FIXME: entered here to make gpload tests pass
-#
-export LOGNAME=$(whoami)
-
-
 display_ccache_statistics() {
     ccache -s
 }
@@ -22,32 +13,54 @@ regain_ownership_of_working_directory() {
 }
 
 
-fetch_and_build_xerces_c() {
+fetch_xerces() {
     git clone https://github.com/greenplum-db/gp-xerces /tmp/xerces
-    (
-	    set -e
-	    cd /tmp/xerces
-	    ./configure CC='ccache gcc' \
-			CXX='ccache g++' \
-			--disable-network
+}
 
+
+install_xerces() {
+    (
+      set -e
+      cd /tmp/xerces
+      ./configure CC='ccache gcc' \
+  		  CXX='ccache g++' \
+		  --disable-network
     )
     make -j 4 -s -C /tmp/xerces
     sudo make install -C /tmp/xerces
 }
 
 
-fetch_and_build_orca() {
-    local orca_code_url
-    orca_code_url=$(
-	sed -E -n -e '/gporca/s,.*https://github.com/greenplum-db/gporca/releases/download/v(([[:digit:]]|\.)+)/bin_orca_centos5_release.tar.gz.*,https://github.com/greenplum-db/gporca/archive/v\1.tar.gz,p' <gpAux/releng/releng.mk
-	)
-    mkdir /tmp/orca
-    wget -O - "${orca_code_url}" | tar zx --strip-components=1 -C /tmp/orca
+fetch_and_build_xerces_c() {
+    fetch_xerces
+    install_xerces
+}
 
+
+get_orca_url() {
+    sed -E -n -e '/gporca/s,.*https://github.com/greenplum-db/gporca/releases/download/v(([[:digit:]]|\.)+)/bin_orca_centos5_release.tar.gz.*,https://github.com/greenplum-db/gporca/archive/v\1.tar.gz,p' <gpAux/releng/releng.mk
+}
+
+
+fetch_orca() {
+    local orca_code_url
+    orca_code_url=$(get_orca_url)
+    mkdir /tmp/orca
+    wget --quiet --output-document - "${orca_code_url}" | \
+	tar zx --strip-components=1 -C /tmp/orca
+}
+
+
+install_orca() {
     cmake -GNinja -H/tmp/orca -B/tmp/orca/build
     ninja -C /tmp/orca/build
     sudo ninja install -C /tmp/orca/build
+}
+
+
+fetch_and_build_orca() {
+    fetch_orca
+    install_orca
     sudo ldconfig
 }
 
@@ -79,22 +92,33 @@ install_demo_cluster() {
     source ./gpAux/gpdemo/gpdemo-env.sh
 }
 
+start_sshd() {
+    /start-sshd.bash
+}
+
+
+run_make_task() {
+    PGOPTIONS="${PGOPTIONS}" make ${MAKE_TASK}
+}
+
 
 _main() {
+    export GPDB_INSTALL_DIR="$PWD/greenplum-db-devel/"
+    export CCACHE_DIR="$PWD/.ccache"
+    # FIXME: entered here to make gpload tests pass
+    export LOGNAME=$(whoami)
+    
     regain_ownership_of_working_directory
 
     display_ccache_statistics
-    
     fetch_and_build_xerces_c
     fetch_and_build_orca
     install_gpdb
-
     display_ccache_statistics
-    
-    /start-sshd.bash
+    start_sshd
     install_demo_cluster
-    
-    PGOPTIONS="${PGOPTIONS}" make ${MAKE_TASK};
+
+    run_make_task
 }
 
 
