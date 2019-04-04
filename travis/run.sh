29d7cac70760014cd24ea5eea00963cff2056f64
Adam Berlin <aberlin@pivotal.io>
2019-04-04 04:01:05 +0000

    try running with a task a different way.

--- travis/run.sh
+++ travis/run.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-export MAKE_TASK='-C src/test/regress installcheck'
-docker run -v ${PWD}:/gpdb_src --workdir=/gpdb_src --rm pivotaldata/greenplum-server-ubuntu-18 ./concourse/pr_pipeline/runner.bash
+docker run \
+       -v ${PWD}:/gpdb_src \
+       --workdir=/gpdb_src \
+       --rm pivotaldata/greenplum-server-ubuntu-18 \
+       MAKE_TASK='-C src/test/regress installcheck' ./concourse/pr_pipeline/runner.bash
