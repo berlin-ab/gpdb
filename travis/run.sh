6edc3f07ff8a0cb43c50d3dc4b4633b438264736
Adam Berlin <aberlin@pivotal.io>
2019-04-04 13:40:07 +0000

    Prime the cache.

--- travis/run.sh
+++ travis/run.sh
@@ -3,6 +3,5 @@
 docker run \
        -v ${PWD}:/gpdb_src \
        --workdir=/gpdb_src \
-       --env MAKE_TASK='-C src/test/regress installcheck' \
        --rm pivotaldata/greenplum-server-ubuntu-18 \
        ./concourse/pr_pipeline/runner.bash
